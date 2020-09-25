5ae66386740c95fce601c07d4865d505b5ba9c3e
MWLeonel <wmleonel6@gmail.com>
2020-09-25 00:23:58 -0300

    Avanço de 6Hrs e 30min

---
+++ MaisTarde/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MaisTarde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime horaAtual;
+            DateTime noite;
+
+            string horaCurta;
+            string noiteCurta;
+
+            horaAtual = DateTime.Now;
+            horaCurta = horaAtual.ToShortTimeString();
+            Console.WriteLine("Hora Atual");
+            Console.WriteLine(horaCurta);
+            Console.Write("\n");
+
+            noite = horaAtual.AddHours(6).AddMinutes(30);
+            noiteCurta = noite.ToShortTimeString();
+            Console.WriteLine("Mais Tarde");
+            Console.WriteLine(noiteCurta);
+            Console.Write("\n");
+
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
